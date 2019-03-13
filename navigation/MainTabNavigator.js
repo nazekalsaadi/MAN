415720dcd1e1ebb91ca16c5c2cb5aaefd5e092d6
Nazek Amin AlSaadi <60084228@cna-qatar.edu.qa>
2019-03-13 12:08:35 +0300

    first commit again

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import Map from '../screens/MapScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import Testing from '../screens/Testing';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const Mapstack = createStackNavigator({
+  Map: Map,
+});
+
+Mapstack.navigationOptions = {
+  tabBarLabel: 'Map',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+
+export default createBottomTabNavigator({
+  HomeStack,
+  Mapstack,
+  SettingsStack,
+});
